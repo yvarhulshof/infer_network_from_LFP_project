6d84198c69d00c5e7bfd3c660404ebeb72fc2bd1
Kris Evers <krisevers14@gmail.com>
2022-10-05 08:52:01 +0200

    Add currentmeter

--- simulate_network.py
+++ simulate_network.py
@@ -5,7 +5,7 @@ def running_mean(x, N):
     cumsum = np.cumsum(np.insert(x, 0, 0))
     return (cumsum[N:] - cumsum[:-N]) / float(N)
 
-def run_simulation(t_sim=1000, stim_strength=0., stim_start=100, stim_dur=100, population=0, seed=55):
+def run_simulation(t_sim=1000, stim_strength=0., stim_start=100, stim_dur=100, population=0, seed=55, output='I'):
     """
     Run a simulation to obtain data
 
@@ -31,6 +31,8 @@ def run_simulation(t_sim=1000, stim_strength=0., stim_start=100, stim_dur=100, p
     import numpy as np
     import os
 
+    import IPython
+
     sim_dict['rng_seed'] = seed
     sim_dict['data_path'] = os.path.join(os.getcwd(), 'data/')
 
@@ -53,40 +55,66 @@ def run_simulation(t_sim=1000, stim_strength=0., stim_start=100, stim_dur=100, p
     plt.figure()
     for i in range(8):
         print('\tPopulation {}/8'.format(i+1))
-        datContent = [i.strip().split() for i in open("data/voltmeter-{}-0.dat".format(7718+i)).readlines()]
-        dat_ = np.array(datContent[3:]).astype(float)
+        if output == 'V_m':
+            datContent = [i.strip().split() for i in open("data/voltmeter-{}-0.dat".format(7718+i)).readlines()]
+            dat_ = np.array(datContent[3:]).astype(float)
 
-        min_t = np.min(dat_[:,1])
-        max_t = np.max(dat_[:,1])
+            min_t = np.min(dat_[:, 1])
+            max_t = np.max(dat_[:, 1])
 
-        V_m = np.zeros(int(max_t-min_t)+1)   # if resolution (dt) = 1 ms
-        times = np.arange(min_t, max_t+1)
+            V_m = np.zeros(int(max_t - min_t) + 1)  # if resolution (dt) = 1 ms
 
-        for i in range(np.shape(dat_)[0]):
-            t_idx = int(dat_[i,1] - min_t)
-            V_m[t_idx] += dat_[i, 2]
-        N = len(np.unique(dat_[:,0]))
-        V_m /= N
+            for i in range(np.shape(dat_)[0]):
+                t_idx = int(dat_[i, 1] - min_t)
+                V_m[t_idx] += dat_[i, 2]
+            N = len(np.unique(dat_[:, 0]))
+            V_m /= N
 
-        # low-pass filter from 1.0 ms to 3.0 ms (LFP is the low-pass filtered signal cutoff at 300 Hz)
-        V_m = running_mean(V_m, 3)
+            # low-pass filter from 1.0 ms to 3.0 ms (LFP is the low-pass filtered signal cutoff at 300 Hz)
+            V_m = running_mean(V_m, 3)
 
-        data.append(V_m)
+            data.append(V_m)
+
+        if output == 'I':
+            # excitatory current input
+            datContent = [i.strip().split() for i in open("data/currentmeter_E-{}-0.dat".format(7726+i)).readlines()]
+            dat_E = np.array(datContent[3:]).astype(float)
+            # inhibitory current input
+            datContent = [i.strip().split() for i in open("data/currentmeter_I-{}-0.dat".format(7734+i)).readlines()]
+            dat_I = np.array(datContent[3:]).astype(float)
+
+            min_t = np.min(dat_E[:, 1])
+            max_t = np.max(dat_E[:, 1])
+
+            I = np.zeros(int(max_t - min_t) + 1)  # if resolution (dt) = 1 ms
+
+            for i in range(np.shape(dat_E)[0]):
+                t_idx = int(dat_E[i, 1] - min_t)
+                I[t_idx] += dat_E[i, 2]
+                I[t_idx] += dat_I[i, 2]
+            N = len(np.unique(dat_E[:, 0]))
+            I /= N
+
+            # low-pass filter from 1.0 ms to 3.0 ms (LFP is the low-pass filtered signal cutoff at 300 Hz)
+            I = running_mean(I, 3)
+
+            data.append(I)
 
     return np.array(data)
 
-def plot_LFP(data):
+def plot_data(data):
     plt.figure(figsize=(4, 4))
     for i in range(len(data)):
         mean = np.mean(data[i])
-        plt.plot(running_mean(data[i], 3) - mean + 10 * i, color='black')
+        plt.plot(data[i] - mean + np.var(data) * i, color='black')
     plt.xlim(left=0, right=1000)
     plt.ylabel(r'$V_m$ [mV]')
     plt.xlabel(r'$Time$ [ms]')
     plt.yticks([])
     plt.tight_layout()
     plt.show()
-    
+
+
 def evaluate(W):
     """
     Parameters
@@ -113,6 +141,6 @@ def evaluate(W):
 
 if __name__ == "__main__":
 
-    data = run_simulation(t_sim=1000, stim_strength=10, stim_start=100, stim_dur=10, population=0)
+    data = run_simulation(t_sim=1000, stim_strength=10, stim_start=100, stim_dur=10, population=0, output='V_m')
 
-    plot_LFP(data)
+    plot_data(data)
